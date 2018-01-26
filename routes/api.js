a1de7d444585c0af511283440a9b96d566b0cc5b
Marco Araujo <sr.marquinho@gmail.com>
2018-01-26 15:02:46 +0000

    add find and delete messages

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET messages */
+/* GET all messages */
 router.get('/messages', getCollection, function(req, res) {
   res.locals.collection.find({}, {}, function(err, doc) {
-    if (err) {
-      sendJsonError(res, "Error: There was a problem with your request");
+    if (err || doc == null) {
+      sendJsonError(res);
     } else {
-      sendJsonSuccess(res, doc);
+      sendJsonMessageSuccess(res, doc);
     }
   });
 });
 
-/* POST messages */
+/* POST message */
 router.post('/messages', getCollection, validateMessage, function(req, res) {
   res.locals.collection.insert({
     "content": req.body.content,
     "timestamp": req.body.timestamp,
     "tags": req.body.tags
   }, function(err, doc) {
-    if (err) {
-      sendJsonError(res, "Error: There was a problem with your request");
+    if (err || doc == null) {
+      sendJsonError(res);
     } else {
-      sendJsonSuccess(res, doc);
+      sendJsonMessageSuccess(res, doc);
+    }
+  });
+});
+
+/* GET message */
+router.get('/messages/:id', getCollection, function(req, res) {
+  res.locals.collection.findOne({_id: req.params.id}, {}, function(err, doc) {
+    if (err || doc == null) {
+      sendJsonError(res);
+    } else {
+      sendJsonMessageSuccess(res, doc);
+    }
+  });
+});
+
+/* DELETE message */
+router.delete('/messages/:id', getCollection, function(req, res) {
+  res.locals.collection.findOneAndDelete({_id: req.params.id}, {}, function(err, doc) {
+    if (err || doc == null) {
+      sendJsonError(res);
+    } else {
+      sendJsonSuccess(res, "Message deleted");
     }
   });
 });
@@ -39,7 +61,7 @@ function validateMessage (req, res, next) {
 }
 
 // helper functions
-function sendJsonSuccess(res, data) {
+function sendJsonMessageSuccess(res, data) {
   res.json({
     "result": true,
     "dataType": "message",
@@ -47,7 +69,16 @@ function sendJsonSuccess(res, data) {
   });
 }
 
+function sendJsonSuccess(res, data) {
+  res.json({
+    "result": true,
+    "dataType": "string",
+    "data": data
+  });
+}
+
 function sendJsonError(res, data) {
+  data = data || "Error: There was a problem with your request";
   res.json({
     "result": false,
     "dataType": "string",
